437ea4fe4a8374da908d1c65f121e5936d607e93
Adarsh <adarshan_naiynar@icloud.com>
2021-04-23 14:26:01 -0700

    Committing frontend

---
+++ frontend/src/MainDisaggregation/Chart.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { VictoryChart, VictoryLine, VictoryZoomContainer, VictoryBrushContainer, VictoryAxis } from 'victory';
+
+const Chart = ({ values, xAxis }) => {
+  const [selectedDomain, setSelectedDomain] = useState(null);
+  const [zoomDomain, setZoomDomain] = useState(null);
+
+  return (
+    <div>
+      <VictoryChart
+        width={550}
+        height={300}
+        scale={{x: "time"}}
+        containerComponent={
+          <VictoryZoomContainer responsive={false}
+            zoomDimension="x"
+            zoomDomain={zoomDomain}
+            onZoomDomainChange={setSelectedDomain}
+          />
+        }
+      >
+        <VictoryAxis
+          tickCount={5}
+          tickValues={xAxis}
+        />
+        <VictoryLine
+          style={{
+            data: {stroke: "tomato"}
+          }}
+          data={values}
+        />
+      </VictoryChart>
+      <VictoryChart
+        width={550}
+        height={90}
+        scale={{x: "time"}}
+        padding={{top: 0, left: 50, right: 50, bottom: 30}}
+        containerComponent={
+          <VictoryBrushContainer responsive={false}
+            brushDimension="x"
+            brushDomain={selectedDomain}
+            onBrushDomainChange={setZoomDomain}
+          />
+        }
+      >
+        <VictoryAxis
+          tickCount={5}
+          tickValues={xAxis}
+        />
+        <VictoryLine
+          style={{
+            data: {stroke: "tomato"}
+          }}
+          data={values}
+        />
+      </VictoryChart>
+    </div>
+  );
+};
+
+export default Chart;
